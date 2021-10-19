239a58a0d75e80a0469e118b8847ac34cce85e9e
quannguyen2306-ctrl <65434894+quannguyen2306-ctrl@users.noreply.github.com>
2021-10-19 10:58:38 +0700

    hehex2

--- snake.py
+++ snake.py
@@ -8,57 +8,59 @@ arr = []
 
 #vẽ cái thân
 def drawLine(lowest,largest, constant):
-    pen.penup()
-    pen.goto(lowest,constant)
-    pen.color('red')
-    for x in range (lowest, largest):
-        pen.pendown()
-        y=10*math.sin(5*math.radians(x)) + constant
-        pen.goto(x,y)
-        if x == largest-1:
-            arr.append(x)
-            arr.append(y)
-            continue 
+    for i in range(2):
+        arr.append(constant)
+        pen.penup()
+        pen.goto(lowest,constant)
+        pen.color('red')
+        for x in range (lowest, largest):
+            pen.pendown()
+            y=10*math.sin(3*math.radians(x)) + constant
+            pen.goto(x,y)
+            if x == largest-1:
+                arr.append(x)
+                arr.append(y)
+                continue 
+        constant = constant -10
 
 #vẽ cái lưỡi
 def drawTonge(smallest, biggest, cons):
     for i in range (smallest, biggest):
         pen.pendown()
-        u=4*math.sin(15*math.radians(i)) + cons
+        u=4*math.sin(10*math.radians(i))+ cons
         pen.goto(i,u)
 
 #vẽ cái đầu
-def drawcircle(r):
+def drawcircle(r, l):
     pen.color("red", "red")
     pen.begin_fill()
     pen.right(120)
-    for loop in range(2):
+    for loop in range(l):
         pen.circle(r,90)
         pen.circle(r/2,90)
     # pen.circle(r,120)
     # pen.right(90)
+    
     pen.end_fill()
     
-def drawSnake():
-    
-    drawLine(-150,125, 50)
-    drawLine(-150,125, 40)
+def drawSnake(start, end, point):
+    drawLine(start,end, point)
     print(arr)
-    x1 = arr[0]
-    y1 = arr[1]
-    x2 = arr[2]
-    y2 = arr[3]
-    midpoint = x2,y2
-    print(midpoint)
+    const = arr[0]-5
+    x2 = arr[4]
+    y2 = arr[5]
+    startpoint = x2,y2
     pen.penup()
-    pen.goto(midpoint)
+    pen.goto(startpoint)
     pen.pendown()
-    drawcircle(20)
-    pen.penup()
-    pen.goto(x1,y1)
-    drawTonge(125, 180, 43)
+    drawcircle(20, 2)
+    print(pen.xcor(), pen.ycor())
+    newx = int(pen.xcor())
+    drawTonge(newx, newx+50, const)
+    
+    # drawTonge(145, 180, 43)
 
-drawSnake()
+drawSnake(-100, 100, 0)
 pen.penup()
 pen.home()
 turtle.done()
